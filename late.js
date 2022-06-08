fbdba84cbebfd7276a6d46817b687e0f3d778c4d
Yoofi <yofs@techexchange.in>
2022-06-08 21:50:08 +0000

    checking time func

--- late.js
+++ late.js
@@ -1,1 +1,13 @@
 // start coding your function here!
+function running_late(date) {
+    var hours = date.getHours(); // get hours of the day
+    if (hours >= 22) {
+        return "It is late!";
+    } else {
+        return "It is still early!";
+    }
+}
+// new Date() returns the current date and time
+console.log(running_late(new Date())); 
+
+exports.running_late = running_late;
